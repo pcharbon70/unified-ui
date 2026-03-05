5c68caecbbad18fc331f4b03bb90eea21d4f9209
Pascal Charbonneau <pcharbon70@gmail.com>
2026-03-05 08:48:27 -0700

    test: add UnifiedUi.Agent error-path coverage

--- test/unified_ui/agent_test.exs
+++ test/unified_ui/agent_test.exs
@@ -72,6 +72,44 @@ defmodule UnifiedUi.AgentTest do
     end
   end
 
+  describe "error paths" do
+    test "returns :not_found for unknown component ids" do
+      component_id = :missing_component
+
+      assert {:error, :not_found} = Agent.whereis(component_id)
+      assert {:error, :not_found} = Agent.signal_component(component_id, %{type: "inc"})
+      assert {:error, :not_found} = Agent.current_state(component_id)
+      assert {:error, :not_found} = Agent.current_iur(component_id)
+      assert {:error, :not_found} = Agent.render_results(component_id)
+      assert {:error, :not_found} = Agent.stop_component(component_id)
+    end
+
+    test "start_component/3 returns already_started when component id is reused" do
+      component_id = :counter_component_duplicate
+      assert {:ok, pid} = Agent.start_component(CounterComponent, component_id)
+
+      on_exit(fn ->
+        Agent.stop_component(component_id)
+      end)
+
+      assert {:error, {:already_started, ^pid}} =
+               Agent.start_component(CounterComponent, component_id)
+    end
+
+    test "start_component/3 returns runtime_not_started when runtime supervisor is stopped" do
+      with_runtime_child_stopped(UnifiedUi.AgentSupervisor, fn ->
+        assert {:error, :agent_runtime_not_started} =
+                 Agent.start_component(CounterComponent, :runtime_down_start)
+      end)
+    end
+
+    test "stop_component/1 returns runtime_not_started when registry is stopped" do
+      with_runtime_child_stopped(UnifiedUi.AgentRegistry, fn ->
+        assert {:error, :agent_runtime_not_started} = Agent.stop_component(:runtime_down_stop)
+      end)
+    end
+  end
+
   defp assert_component_stopped(component_id, attempts \\ 20)
 
   defp assert_component_stopped(_component_id, 0) do
@@ -86,6 +124,16 @@ defmodule UnifiedUi.AgentTest do
       {:ok, _pid} ->
         Process.sleep(10)
         assert_component_stopped(component_id, attempts - 1)
+    end
+  end
+
+  defp with_runtime_child_stopped(child_id, fun) do
+    assert :ok = Supervisor.terminate_child(UnifiedUi.Supervisor, child_id)
+
+    try do
+      fun.()
+    after
+      assert {:ok, _pid} = Supervisor.restart_child(UnifiedUi.Supervisor, child_id)
     end
   end
 end
